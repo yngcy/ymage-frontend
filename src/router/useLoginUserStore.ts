459fc0a30e85d744fab5037aa726383110eedf39
yngcy <2792783811@qq.com>
2026-03-18 17:47:34 +0800

    first commit

---
+++ src/router/useLoginUserStore.ts
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useLoginUserStore = defineStore("loginUser", () => {
+  const loginUser = ref<any>({
+    userName: "未登录",
+  });
+
+  async function fetchLoginUser() {
+    // todo 由于后端还没提供接口，暂时注释
+    // const res = await getCurrentUser();
+    // if (res.data.code === 0 && res.data.data) {
+    //   loginUser.value = res.data.data;
+    // }
+  }
+
+  function setLoginUser(newLoginUser: any) {
+    loginUser.value = newLoginUser;
+  }
+
+  return { loginUser, setLoginUser, fetchLoginUser };
+});
